9d977e6c85004d727e0caae7955611447607204d
selenkunkcu <selenkunkcu@gmail.com>
2026-03-26 16:03:28 +0300

    NativeWind setup and assets addition

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+                Welcome to Nativewind!
+            </Text>
+        </View>
+    );
 }
