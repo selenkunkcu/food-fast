1ca5c492edf8b784c63611c1e9f86c1c91b8b94e
selenkunkcu <selenkunkcu@gmail.com>
2026-03-27 11:14:06 +0300

    Appwrite setup

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.selen.foodfast",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
