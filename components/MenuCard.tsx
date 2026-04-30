51307cd554cfbb706882095d14515e0beb2d5ae5
selenkunkcu <selenkunkcu@gmail.com>
2026-04-30 13:42:15 +0300

    cart screen

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,8 +1,10 @@
 import {Text, TouchableOpacity, Image, Platform} from "react-native";
 import {MenuItem} from "@/type";
 import {appwriteConfig} from "@/lib/appwrite";
+import {useCartStore} from "@/store/cart.store";
 
-const MenuCard = ({ item: { image_url, name, price }}: { item: MenuItem }) => {
+const MenuCard = ({ item: { $id, image_url, name, price }}: { item: MenuItem }) => {
+    const { addItem } = useCartStore();
 
     return (
         <TouchableOpacity className="menu-card" style={Platform.OS == "android" ? { elevation: 10, shadowColor: "#878787" } : {}}>
@@ -10,7 +12,7 @@ const MenuCard = ({ item: { image_url, name, price }}: { item: MenuItem }) => {
                    onError={(e) => console.log("Image load error:", e.nativeEvent.error)}/>
             <Text className="text-center base-bold text-dark-100 mb-2" numberOfLines={1}>{name}</Text>
             <Text className="body-regular text-gray-200 mb-4"> From ${price}</Text>
-            <TouchableOpacity onPress={() => {}}>
+            <TouchableOpacity onPress={() => addItem({ id: $id, name, price, image_url: image_url, customizations: [] })}>
                 <Text className="paragraph-bold text-primary">Add to Cart +</Text>
             </TouchableOpacity>
         </TouchableOpacity>
